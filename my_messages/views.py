d610f3ea9d59a142543b7f465fd0e89b74c3f734
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-06-19 12:09:54 +0300

    видалення повідомлень, модальні вікна

--- my_messages/views.py
+++ my_messages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from chat.models import Message, Chat
 from django.contrib.auth.models import User
 from django.db.models import Q
@@ -6,4 +6,11 @@ from django.db.models import Q
 def my_messages(request):
     user = request.user
     chats = Chat.objects.filter(Q(user1=user) | Q(user2=user))
+    if request.method == 'POST' and chats:
+        if request.POST.get('delete'):
+            chat_id = request.POST.get('delete')
+            chat = Chat.objects.get(id = chat_id)
+            chat.delete()
+            return redirect('my_messages')
+
     return render(request, 'my_messages/my_messages.html', context={'all_messages': chats})
