6e4296b859c12ea9777296bdb5544d96701a0573
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-06-24 22:16:41 +0300

    адаптація

--- LinkLounge/asgi.py
+++ LinkLounge/asgi.py
@@ -1,7 +1,7 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application # створює ASGI-додаток для обробки HTTP-запитів у Django.
+from channels.auth import AuthMiddlewareStack # забезпечує обробку аутентифікації для WebSocket-з'єднань.
+from channels.routing import ProtocolTypeRouter, URLRouter #  використовуються для маршрутизації запитів різних типів (HTTP, WebSocket) до відповідних обробників.
 import chat.routing  # Імпорт маршрутів URL для WebSocket для додатку чату
 import groups.routing  # Імпорт маршрутів URL для WebSocket для додатку груп
 
--- chat/routing.py
+++ chat/routing.py
@@ -4,3 +4,11 @@ from . import consumers
 websocket_urlpatterns = [
     path('ws/chat/<int:chat_id>/', consumers.ChatConsumer.as_asgi()),
 ]
+# websocket_urlpatterns: Це список, який містить маршрути URL для WebSocket-з'єднань. 
+# Цей список буде використовуватися URLRouter для маршрутизації WebSocket-запитів.
+# path('ws/chat/int_id>/', consumers.ChatConsumer.as_asgi()): Визначає маршрут URL для WebSocket-з'єднання.
+# 'ws/chat/int_id>/': Вказує URL-шаблон для WebSocket-з'єднання. У цьому випадку, WebSocket-з'єднання буде встановлюватися за URL, 
+# що починається з 'ws/chat/', і містить параметр chat_id. Цей параметр буде передаватися у WebSocket-споживача для обробки.
+# consumers.ChatConsumer.as_asgi(): Вказує, що запити за цим URL будуть оброблятися споживачем ChatConsumer, 
+# який визначений у модулі consumers. 
+# Виклик as_asgi() перетворює клас споживача у ASGI-додаток, який може обробляти WebSocket-запити.
