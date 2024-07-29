7ae01a6770915334d315e98b42edd994b5de9308
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-07-29 19:57:10 +0300

    allowed hosts

--- LinkLounge/settings.py
+++ LinkLounge/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['linklounge.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['linklounge.herokuapp.com', 'linklounge-0488d39446ec.herokuapp.com', 'localhost']
 
 INSTALLED_APPS = [
     'daphne',
