7ac884a3c2ed1e4e06b457803bbac225cf3f8db6
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-09-08 20:42:25 +0300

    notification mobile

--- my_messages/static/my_messages/js/modal.js
+++ my_messages/static/my_messages/js/modal.js
@@ -6,11 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const btn_no = document.querySelector('#btn-no');
     const btn_yes = document.querySelector('#btn-yes');
     const modalUsername = document.querySelector('#modal-username');
+    // для сповіщення 
+    let senderId = ''
+    let notif = document.querySelector('.count-notif')
+    let notifMob = document.querySelector('.count-notif-mob')
 
     deleteButtons.forEach(button => {
         button.addEventListener('click', function() {
             const chatId = this.getAttribute('data-chat-id');
             const chatUsername = this.getAttribute('data-chat-username');
+            senderId = Number(this.getAttribute('data-sender-id'));
+            console.log(`перевірка в модальному вікні має бути ІД ${senderId} відправника`)
 
             btn_yes.value = chatId;
             modalUsername.textContent = chatUsername;
@@ -19,6 +25,36 @@ document.addEventListener('DOMContentLoaded', function() {
             popupBg.classList.add('show');
         });
     });
+
+    btn_yes.addEventListener('click', function(){
+        console.log(`натиснули на ні і побачили id ${senderId}`)
+        let users = localStorage.getItem('allMessageUsers');
+        let storedNotifCount = Number(localStorage.getItem('notifCount'));
+    
+        let arrayUsers = users ? JSON.parse(users) : [];
+        console.log(arrayUsers)
+        if (arrayUsers.includes(senderId)){
+            console.log(arrayUsers.includes(senderId))
+            let indexEl = arrayUsers.indexOf(senderId)
+            // console.log(`індекс який я хочу видалити ${indexEl}`)
+            arrayUsers.splice(indexEl, 1)
+            // console.log(`список ${arrayUsers} має бути без ${indexEl}`)
+            localStorage.setItem('allMessageUsers', JSON.stringify(arrayUsers))
+            storedNotifCount -= 1
+            notif.textContent = storedNotifCount
+            notifMob.textContent = storedNotifCount
+            console.log(`${storedNotifCount} стореднотіф каунт`)
+            // notif.textContent = storedNotifCount
+            localStorage.setItem('notifCount', notif.textContent)
+        }
+        if (storedNotifCount < 1) {
+            notif.classList.add('hide');
+            notifMob.classList.add('hide');
+        }
+
+    })
+
+    
 
     const closeModalElements = [popupBg, close_modal_1, btn_no];
     closeModalElements.forEach(function(element) {
