a7b181bab2aa14a9291f53f094900bccd2b10f18
MaxSlipchuk <nice.slipchukmax@gmail.com>
2024-09-17 14:39:25 +0300

    video on mobile

--- static/app_base/js/video-mobile.js
+++ static/app_base/js/video-mobile.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const video = document.querySelector('.background-video');
+    const video = document.querySelector('.video');
 
     if (video && window.innerWidth < 768) {
         video.removeAttribute('autoplay');
         video.pause();
-        console.log('перевырка')
     }
 });
